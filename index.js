2e5d92baf2ededefa70dd08c03d0b86cbb0476af
mahfuj10 <mahfujglobal@gmail.com>
2022-01-02 18:28:11 +0600

    added file

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const fileUpload = require('express-fileupload');
+const ObjectId = require('mongodb').ObjectId;
+
+// middleare
+app.use(fileUpload());
+app.use(cors());
+app.use(express.json());
+
+
+async function run() {
+
+    // mongo db uri
+
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39aol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+    try {
+        await client.connect();
+        const database = client.db("Tourism");
+        const ourDivions = database.collection('divisions');
+        const ourPlaces = database.collection('places');
+        const users = database.collection('users');
+
+
+
+        // saver user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // save user google sign
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+
+    }
+    finally {
+        // await client.close()
+    }
+
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('welcome to bangla tourism');
+})
+
+app.listen(port, () => {
+    console.log(`My server is running`);
+})
