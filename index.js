b67724b4fd6e802b3ab1201c5f004798c7610d2f
mahfuj10 <mahfujglobal@gmail.com>
2022-01-06 17:05:57 +0600

    add new mthod

--- index.js
+++ index.js
@@ -99,6 +99,14 @@ async function run() {
             res.json(result);
         });
 
+        // delete tour place
+        app.delete('/tour/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ourPlaces.deleteOne(query);
+            res.json(result);
+        });
+
         // save user google sign
         app.put('/users', async (req, res) => {
             const user = req.body;
